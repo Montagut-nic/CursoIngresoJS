8178f6aa11c321daf92287172f67ed4ebcb0c55d
Montagut-nic <nico.monta99@gmail.com>
2021-01-30 08:01:03 -0300

    ejercicios 4 y 5 cambios

--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,8 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	var nombre;
+	nombre=prompt('igrese su nombre','su nombre');
+	txtIdNombre.value = nombre;
 }
 
