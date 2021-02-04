d40430c3e81375c6a2408c671502a751cf125f20
Montagut-nic <nico.monta99@gmail.com>
2021-02-04 08:04:12 -0300

    cambios ejercicio 7

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -4,36 +4,47 @@ transformarlos a enteros (parseInt),realizar la operación correcta y
 mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 
-let num1;
-let num2;
-num1 = parseInt(document.getElementById('txtIdNumeroUno').value);
-num2 = parseInt(document.getElementById('txtIdNumeroDos').value);
-
 function sumar()
 {	
-	let suma;
-	suma = num1+num2;
-	alert("El resultado de la suma es "+suma);	
+	let num1;
+	let num2;
+	let resultado;
+	num1 = parseInt(document.getElementById('txtIdNumeroUno').value);
+	num2 = parseInt(document.getElementById('txtIdNumeroDos').value);
+	resultado = num1+num2;
+	alert("El resultado de la suma es "+resultado);	
 }
 
 function restar()
 {
-	let resta;
-	resta=num1-num2;
-	alert ("El resultado de la resta es "+resta);
+	let num1;
+	let num2;
+	let resultado;
+	num1 = parseInt(document.getElementById('txtIdNumeroUno').value);
+	num2 = parseInt(document.getElementById('txtIdNumeroDos').value);
+	resultado=num1-num2;
+	alert ("El resultado de la resta es "+resultado);
 }
 
 function multiplicar()
 { 
-	let multiplicación;
-	multiplicación=num1*num2;
-	alert ("El resultado de la multiplicación es "+multiplicación);
+	let num1;
+	let num2;
+	let resultado;
+	num1 = parseInt(document.getElementById('txtIdNumeroUno').value);
+	num2 = parseInt(document.getElementById('txtIdNumeroDos').value);
+	resultado=num1*num2;
+	alert ("El resultado de la multiplicación es "+resultado);
 }
 
 function dividir()
 {
-	let división;
-	división=num1/num2;
-	alert ("El resultado de la división es "+división);
+	let num1;
+	let num2;
+	let resultado;
+	num1 = parseInt(document.getElementById('txtIdNumeroUno').value);
+	num2 = parseInt(document.getElementById('txtIdNumeroDos').value);
+	resultado=num1/num2;
+	alert ("El resultado de la división es "+resultado);
 }
 
